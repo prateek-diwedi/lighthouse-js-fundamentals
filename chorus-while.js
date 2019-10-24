3661e8ca6834a95e84787fba32fc04b334e5dbaf
Prateek Diwedi <diwedi1112@gmail.com>
2019-10-24 06:30:24 +0000

    Use a while loop to create a chorus line

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Let's Dance!";
+let repeat = 0;
+while (repeat < 10){
+  if(repeat === 5){
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++
+}
+console.log("Until the sun comes up!")
