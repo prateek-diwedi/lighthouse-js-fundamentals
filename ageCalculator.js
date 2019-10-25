9b0c55bfec297324d0c934032ea10f33f662c317
Prateek Diwedi <diwedi1112@gmail.com>
2019-10-25 14:01:57 +0000

    age calculator using functions by  passing 3 parameters

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, start, end){
+  var result;
+  result = end - start;
+return (name + " is " + result + " years old.")
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
